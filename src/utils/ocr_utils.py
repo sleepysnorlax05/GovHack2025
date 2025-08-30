f518baa4477f6970866fbbc757439804f8b86d8a
sleepy-snorlax <k62.2312450081@ftu.edu.vn>
2025-08-30 13:57:17 +1000

    <feat> image parser

--- src/utils/ocr_utils.py
+++ src/utils/ocr_utils.py
@@ -1,3 +1,4 @@
+import re
 import pytesseract
 from PIL import Image
 
@@ -5,3 +6,10 @@ def extract_text_from_image(image_file):
     image = Image.open(image_file).convert("RGB")
     text = pytesseract.image_to_string(image)
     return text
+
+def parse_contact_info(text):
+    email_pattern = r'[\w\.-]+@[\w\.-]+'
+    phone_pattern = r'\+?\d[\d \-\(\)]{7,}\d'
+    emails = re.findall(email_pattern, text)
+    phones = re.findall(phone_pattern, text)
+    return emails, phones
